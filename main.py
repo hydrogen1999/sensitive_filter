3ba383579c11fbe0626f7b8a8ceceea7fb42f8de
longdz <nguyenlongqbtt@gmail.com>
2022-12-01 14:27:47 +0700

    using regex

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ import nltk
 import re
 import string
 import pickle
-import json
 from fastapi import FastAPI 
-from ultils import remove_dirty_word, remove_accent
+from ultils import CheckBadWords, remove_accent
 
 app = FastAPI(
     title="Sensitive content filter API",
@@ -14,16 +13,16 @@ app = FastAPI(
     version="0.1",
 )
 # load lib sensitive
-with open('./vietnam-sensitive-words/bad_words.json',  encoding="utf8") as f:
-    bad_words = json.load(f)
+with open('./vietnam-sensitive-words/profanity_wordlist.txt', 'r', encoding="utf8") as f:
+    list_bad_words = [word[:-1] for word in f]
+with open('./vietnam-sensitive-words/block_words.txt', 'r', encoding="utf8") as f:
+    block_words = [word[:-1] for word in f]
 with open('./domain_shorteners/domain_shorteners.txt', 'r') as f:
     domain_shorteners = [domain[:-1] for domain in f]
 with open('./scam-links/links.txt', 'r') as f:
     scam_links = [link[:-1] for link in f]
 scam = scam_links + domain_shorteners
-
-bad_words_noaccent = [remove_accent(key) for key in bad_words.keys()]
-list_bad_words = bad_words_noaccent + list(bad_words.keys())
+list_bad_words = list_bad_words + scam
 
 # load the sentiment model
 model = keras.models.load_model('./checkpoints/model_name.h5')
@@ -55,31 +54,35 @@ def embed(corpus):
     return word_tokenizer.texts_to_sequences(corpus)
 
 @app.get("/predict-text")
-def predict_sentiment(text: str):
+def predict_sentiment(text: str, AI: int):
     """
     A simple function that receive a text content and predict the sentiment of the content.
     :param text:
     :return: prediction, probabilities
     """
-    cleaned_text, y_pred, y_prob = remove_dirty_word(scam, domain_shorteners, list_bad_words, text)
-    if y_pred == 1:
-        result = {"cleaned_text": cleaned_text, "Sensitive": y_pred, "Score": y_prob}
+    badwords_censor = CheckBadWords(list_bad_words, block_words)
+    text = badwords_censor(text)
+    if re.search("\*", text):
+        result = {"cleaned_text": text, "Sensitive": 1, "Score": 0.99}
         return result
     else:
-        # clean the text
-        cleaned_text = preprocess_data(text)
-        input_model =  pad_sequences(
-                embed([cleaned_text]), 
-                219
-                )
-        
-        # perform prediction
-        y_pred = model.predict(input_model).astype("int32")[0][0].tolist()
-        y_prob = 0.8
-        
-        # output dictionary
-        sentiments = {0: "Negative", 1: "Positive"}
-        
-        # show results
-        result = {"cleaned_text": cleaned_text, "Sensitive": y_pred, "Score": y_prob}
+        if AI == 1:
+            # # clean the text
+            cleaned_text = preprocess_data(text)
+            input_model =  pad_sequences(
+                    embed([cleaned_text]), 
+                    219
+                    )
+            
+            # perform prediction
+            y_pred = model.predict(input_model).astype("int32")[0][0].tolist()
+            y_prob = 0.8
+            
+            # output dictionary
+            sentiments = {0: "Negative", 1: "Positive"}
+            
+            # show results
+            result = {"cleaned_text": cleaned_text, "Sensitive": y_pred, "Score": y_prob}
+        else:
+            result = {"cleaned_text": text, "Sensitive": 0, "Score": 0.99}
         return result
--- ultils.py
+++ ultils.py
@@ -2,35 +2,20 @@ import unidecode
 import re
 from re import search
 
-def check_bad_words(list_bad_words, sentence):
-    remove = []
-    for key in list_bad_words:
-        if re.search(r"\b{}\b".format(key), sentence.lower().strip()):
-            remove.append(key)
-    return remove 
-def check_url(scam, domain_shorteners, sentence):
-    remove = []
-    for domain in scam:
-        if search(domain, sentence):
-            remove.append(domain)
-    for domain in domain_shorteners:
-        if search(domain, sentence):
-            remove.append(domain)
-    return remove 
+
 def remove_accent(sentence):
     return unidecode.unidecode(sentence)
 
-def remove_dirty_word(scam, domain_shorteners, list_bad_words, sentence):
-    if check_url(scam, domain_shorteners, sentence) != []:
-        replace_text = check_url(scam, domain_shorteners, sentence)
+class CheckBadWords:
+    def __init__(self, badwords, blockwords):
+        self.pattern1 = '|'.join([r"\b" + badword + r"\b" for badword in badwords])
+        self.pattern2 = '|'.join(blockwords)
+    def __call__(self, str_):
+        print(str_)
+        pattern1 = re.compile(self.pattern1, re.IGNORECASE)
+        out = pattern1.sub("***", str_)
+        pattern2 = re.compile(self.pattern2, re.IGNORECASE)
+        replace_text = pattern2.findall(str_)
         for word in replace_text:
-            sentence = sentence.replace(word,'*'*len(word)) 
-    if check_bad_words(list_bad_words, sentence) != []:
-        replace_text = check_bad_words(list_bad_words, sentence)
-        for word in replace_text:
-            sentence = sentence.replace(word,'*'*len(word)) 
-    if replace_text != []:
-        y_pred = 1
-    else:
-        y_pred = 0
-    return sentence, y_pred, 0.99
+            out = out.replace(word,'*'*len(word))
+        return out
