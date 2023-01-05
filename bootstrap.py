6b56c18df45b4c552b40cc0cb68251b135035d05
Hùng <33273948+ndgnuh@users.noreply.github.com>
2023-01-05 12:46:17 +0700

    Update bootstrap.py

--- bootstrap.py
+++ bootstrap.py
@@ -1,4 +1,7 @@
+from transformers import AutoModel
 import gdown
+
+AutoModel.from_pretrained("vinai/phobert-base")
 
 # Download the sensitive model
 url = 'https://drive.google.com/u/0/uc?id=1P_x0jQlxTrbeOCatv2xYtDqMyPfgrxLk&export=download'
